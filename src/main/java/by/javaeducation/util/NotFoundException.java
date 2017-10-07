59de89aad22642425a55ac288d91d7bc8a75af67
Yury Mitsiuk <yriymitsuik@gmail.com>
2017-10-07 16:13:57 +0300

    Add ValidationUtil

---
+++ src/main/java/by/javaeducation/util/NotFoundException.java
@@ -0,0 +1,7 @@
+package by.javaeducation.util;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/by/javaeducation/util/ValidationUtil.java
@@ -0,0 +1,34 @@
+package by.javaeducation.util;
+
+import by.javaeducation.HasID;
+
+public class ValidationUtil {
+
+    private ValidationUtil() {
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        return checkNotFound(object, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String arg) {
+        if (!found) {
+            throw new NotFoundException(arg);
+        }
+    }
+
+    public static void checkNew(HasID bean) {
+        if (!bean.isNew()) {
+            throw new IllegalArgumentException(bean + " must be new (id=null)");
+        }
+    }
+}
